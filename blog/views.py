30c7d3d263f020e02fa82aeeba7f2e4c7a4ec528
meklit12 <meklitfrehun12@gmail.com>
2021-12-29 12:23:26 +0300

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+def list_blogs(request):
+ blogs = Blog.objects.all()
+ return render(request, "blog/list.html", {"blogs": blogs})
+def detail_blog(request, pk):
+ blog = get_object_or_404(Blog, pk=pk)
+ return render(request, "blog/detail.html", {"blog": blog})
