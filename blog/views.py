813ca417c8fe3039b1e38db2ac5ba544668c6353
mihiretab-taye <mihiretab.taye@gmail.com>
2021-12-29 13:43:34 +0300

    view and detail functions added.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+
+def list_blogs(request):
+    blogs = Blog.objects.all()
+    return render(request, "blog/list.html", {"blogs": blogs})
+
+
+def detail_blog(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    return render(request, "blog/detail.html", {"blog": blog})
